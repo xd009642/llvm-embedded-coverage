c15428490e8b2767a8350017cbea93e591475c77
xd009642 <danielmckenna93@gmail.com>
2021-11-22 17:50:49 +0000

    Make buffer size configurable

---
+++ build.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let size = env::var("COVERAGE_BUFFER_SIZE")
+        .map(|x| x.parse::<usize>().unwrap_or(1024))
+        .unwrap_or(1024);
+
+    let output = Path::new(&env::var_os("OUT_DIR").unwrap())
+        .join("constants.rs");
+
+    fs::write(&output, format!("pub const COVERAGE_BUFFER_SIZE: usize = {};", size)).unwrap();
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@
 use core::mem::MaybeUninit;
 use std::fs::write;
 
+#[allow(dead_code)]
+mod constants {
+    include!(concat!(env!("OUT_DIR"), "/constants.rs"));
+}
 
 #[no_mangle]
 #[used]
@@ -19,11 +23,18 @@ extern "C" {
 
 fn main() {
     let length = unsafe { __llvm_profile_get_size_for_buffer()} as usize;
-    
-    let mut buffer: [MaybeUninit<u8>; 512] = MaybeUninit::uninit_array();
-    if length > 512 {
-        panic!("Buffer too small {}>{}", length, 512);
-    }
+
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "alloc")] {
+            let mut buffer = Vec::<MaybeUninit<u8>>::new();
+            buffer.resize(length, MaybeUninit::uninit());
+        } else {
+            let mut buffer: [MaybeUninit<u8>; constants::COVERAGE_BUFFER_SIZE] = MaybeUninit::uninit_array();
+            if length > constants::COVERAGE_BUFFER_SIZE {
+                panic!("Buffer too small {}>{}", length, constants::COVERAGE_BUFFER_SIZE);
+            }
+        }
+    } 
 
     let output = unsafe {
         let res = __llvm_profile_write_buffer(buffer.as_mut_ptr() as *mut u8);
